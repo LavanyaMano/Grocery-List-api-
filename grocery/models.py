dd3627c573b8e37df28a3e85c2bf0bd566a3dfe3
lava m <lavanymkanna@gmail.com>
2016-10-19 13:45:23 -0400

    fixed page refreshing error

--- grocery/models.py
+++ grocery/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class GroceryItem(models.Model):
     name = models.CharField(max_length=100)
-    quantity = models.DecimalField(max_digits=10,decimal_places = 2)
-    price =models.DecimalField(max_digits=10,decimal_places = 2)
+    quantity = models.DecimalField(max_digits=10,decimal_places = 2, default = 1)
+    price =models.DecimalField(max_digits=10,decimal_places = 2,default = 1)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
